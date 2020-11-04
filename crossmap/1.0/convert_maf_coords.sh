7347d11916eb3ebc7ac79c0b6512830484f49d69
Kdreval <k.dreval@gmail.com>
2020-11-04 14:44:53 -0800

    handle conversion to both builds

--- crossmap/1.0/convert_maf_coords.sh
+++ crossmap/1.0/convert_maf_coords.sh
@@ -85,7 +85,7 @@ if [[ ${NUM_UNMAPPED} > 0 ]]; then
 fi
 
 DATE=$(date --rfc-3339 seconds | cut -c1-19)
-echo "[${DATE}] INFO: Converting output BED file (${OUTPUT_BED}) to MAF format." \
+echo "[${DATE}] INFO: Converting output BED file (${OUTPUT_BED}) to MAF format."
 grep "^Hugo_Symbol" "${INPUT_MAF}" > "${OUTPUT_MAF}"
 awk 'BEGIN {FS=OFS="\t"} \
 		{row = gensub(/['"${MAFCOLSEP}"']/, "\t", "g", $4)} \
@@ -98,7 +98,7 @@ awk 'BEGIN {FS=OFS="\t"} \
 		$13 == "INS" {start = start; end = end + 1} \
 		{$8 = chrom; $9 = start; $10 = end; print $0}' \
 	| cut -f4- \
-	| awk 'BEGIN {FS=OFS="\t"} {$4 = "GRCh37"; print $0}' \
+	| awk -v chain="${CHAIN_FILE}" 'BEGIN {OFS="\t"}; { if (chain ~ /hg38ToHg19/) $4 ="GRCh37"; else $4="GRCh38"; print ($0)}' \
 	| sed 's/_____/ /g' >> "${OUTPUT_MAF}"
 
 if [[ ${DEBUG} == 0 ]]; then
