0ce0d1767c16b9d86515a13b5f5d1374aa8a286f
gillissierra <gillissierra@gmail.com>
2023-06-29 14:44:07 -0700

    example run command untested

--- generate_smr_inputs/1.0/generate_smr_inputs.R
+++ generate_smr_inputs/1.0/generate_smr_inputs.R
@@ -5,7 +5,8 @@
 # and prepare it to be used with SMR tools e.g. gistic2
 
 # Usage:
-#   Rscript generate_smg_inputs.R <path/to/master/maf> <path/to/sample_sets> <path/to/output/folder> <case_set> <mode> <include non-coding>
+#   Rscript generate_smr_inputs.R <path/to/master/seg> <path/to/sample_sets> <path/to/output/folder> <case_set> <mode>
+#   Example: generate_smr_inputs.R results/gambl/gistic2-1.0/00-inputs/capture--projection/all-grch37.seg data/metadata/level3_samples_subsets.tsv "FLs_with_LSARP_Trios" gistic2
 #
 # Notes:
 #   Adapted from generate_smg_inputs/1.0/generate_smg_inputs.R
@@ -16,3 +17,52 @@
 #   in a particular subset. The column name for the subset will be used as the naming of the
 #   output seg file at the user-provided location.
 #
+
+# Load packages -----------------------------------------------------------
+message("Loading packages...")
+suppressWarnings(
+suppressPackageStartupMessages({
+  library(data.table)
+  library(tidyverse)
+})
+)
+
+# Determine arguments -----------------------------------------------------
+
+# Parse command-line arguments
+args <- commandArgs(trailingOnly = TRUE) %>% as.list()
+arg_names <- c("master_seg", "all_sample_sets", "output_path", "case_set", "mode")
+# if there are multiple seg files passed, collapse them into one list
+args = c(
+        list(unlist(args[1:(length(args)-5)])),
+        args[(length(args)-4):length(args)]
+        )
+args <- setNames(args, arg_names[1:length(args)])
+args$master_seg = as.list(args$master_seg)
+
+# Print args for debugging
+print(paste("master_seg:",args$master_seg,
+            "all_sample_sets:",args$all_sample_sets,
+            "output_path:",args$output_path,
+            "case_set",args$case_set,
+            "mode",args$mode))
+
+# Ensure consistent naming of the sample ID column-------------------
+if (file.exists(args$all_sample_sets)) {
+  full_case_set = suppressMessages(read_tsv(args$all_sample_sets))
+} else {
+  message(paste("Warning: case set is requested, but the case set file", full_case_set_path, "is not found."))
+  stop("Exiting because did not found case set-defining file")
+}
+
+full_case_set =
+  full_case_set %>% rename_at(vars(matches(
+    "sample_id", ignore.case = TRUE
+  )),
+  ~ "Tumor_Sample_Barcode")
+
+# get case set as defined in the file
+this_subset_samples =
+  full_case_set %>%
+  dplyr::filter(!!sym(args$case_set) == 1) %>%
+  pull(Tumor_Sample_Barcode)
