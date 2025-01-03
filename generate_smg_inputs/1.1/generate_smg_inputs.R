be68e2a66072d52771071cbfb7f6a2fdcfc1b836
gillissierra <gillissierra@gmail.com>
2025-01-03 10:43:02 -0800

    inlcude Tumor_Seq_Allele1 column from maf

--- generate_smg_inputs/1.1/generate_smg_inputs.R
+++ generate_smg_inputs/1.1/generate_smg_inputs.R
@@ -137,7 +137,7 @@ if (mode == "rainstorm"){
 }
 
 # Read in only minimally necessary columns
-relevant_maf_columns <- c("Hugo_Symbol", "Tumor_Sample_Barcode", "NCBI_Build", "Chromosome", "Start_Position", "End_Position", "Strand", "Variant_Classification", "Variant_Type", "Reference_Allele", "Tumor_Seq_Allele2", "HGVSp_Short", "Transcript_ID", "Protein_position")
+relevant_maf_columns <- c("Hugo_Symbol", "Tumor_Sample_Barcode", "NCBI_Build", "Chromosome", "Start_Position", "End_Position", "Strand", "Variant_Classification", "Variant_Type", "Reference_Allele", "Tumor_Seq_Allele1", "Tumor_Seq_Allele2", "HGVSp_Short", "Transcript_ID", "Protein_position")
 
 if ("genome" %in% subsetting_values$seq_type && !("capture" %in% subsetting_values$seq_type)) { # genome only
   cat("Loading genome input file ...\n")
